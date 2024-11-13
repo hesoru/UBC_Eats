024b4a48ba13b09bb84222e2923d8bd02d3a496a
ore082 <ore082@students.cs.ubc.ca>
2024-11-12 22:18:39 -0800

    fixed connection issue

--- appController.js
+++ appController.js
@@ -1,4 +1,5 @@
-const express = require('express');
+//const express = require('express');
+import express from "express";
 const appService = require('./appService');
 
 const router = express.Router();
