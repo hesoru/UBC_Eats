9890d471688f826945e4f65fc946f6d928c5ac26
RitRot2468 <ritrot2468@gmail.com>
2024-11-28 00:07:44 -0800

    worked on ui and adding feedback messages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,28 +14,32 @@ function App() {
 
     return (
         <Router>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1>
+            <div className="App-header bg-blue-50 text-black py-6 px-8 shadow-md">
+                <header className="flex flex-col items-center justify-center text-center space-y-4">
+                    <img src={logo} className="App-logo h-16" alt="logo"/>
+                    <h1 className="text-3xl font-mono">
                         Looking for delicious food that fits your needs?
                     </h1>
-                    <nav>
-                        <Link to="/signup">Sign Up/In</Link> |{' '}
-                        <Link to="/restaurants">Restaurants</Link> |{' '}
-                        <Link to={`/user/${currentUser}`}>User Review</Link> |{' '}
-                        <Link to={`/menu/49.269235/-123.255589`}>Menu (Temp Link)</Link>
-                        <Link to={`/find-food`}>FindFood</Link>
+
+                    <nav className="mt-4 space-x-6">
+                        <Link to="/signup" className="text-lg font-bold font-mono hover:text-indigo-200 transition">Sign Up/In</Link> |{' '}
+                        <Link to="/restaurants"
+                              className="text-lg font-bold font-mono  hover:text-indigo-200 transition">Restaurants</Link> |{' '}
+                        <Link to={`/user/${currentUser}`} className="text-lg font-bold font-mono  hover:text-indigo-200 transition">User
+                            Review</Link> |{' '}
+                        <Link to={`/menu/49.269235/-123.255589`} className="text-lg font-bold font-mono :text-indigo-200 transition">Menu
+                            (Temp Link)</Link> |{' '}
+                        <Link to={`/find-food`} className="text-lg font-bold font-mono hover:text-indigo-200 transition">Find Food</Link>
                     </nav>
                 </header>
 
                 <Routes>
-                    <Route path="/signup" element={<SignUp setCurrentUser={setCurrentUser} />} />
-                    <Route path="/signin" element={<SignIn setCurrentUser={setCurrentUser} />} />
-                    <Route path="/restaurants" element={<RestaurantsContainer />} />
-                    <Route path="/user/:userName" element={<UserReview />} />
-                    <Route path="/menu/:lat/:lon" element={<MenuPage />} />
-                    <Route path="/find-food" element={<FindFood />} />
+                    <Route path="/signup" element={<SignUp setCurrentUser={setCurrentUser}/>}/>
+                    <Route path="/signin" element={<SignIn setCurrentUser={setCurrentUser}/>}/>
+                    <Route path="/restaurants" element={<RestaurantsContainer/>}/>
+                    <Route path="/user/:userName" element={<UserReview/>}/>
+                    <Route path="/menu/:lat/:lon" element={<MenuPage/>}/>
+                    <Route path="/find-food" element={<FindFood/>}/>
                 </Routes>
             </div>
         </Router>
